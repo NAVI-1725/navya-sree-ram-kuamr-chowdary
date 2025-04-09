2fa6f71b6c12e216d7de69b99b041e001586a0d8
NAVI-1725 <navichowdary1725@gmail.com>
2025-04-10 01:12:34 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import "../styles/globals.css"; 
+// app/layout.tsx
+
+import "../styles/globals.css";
 import { ReactNode } from "react";
 
 export const metadata = {
   title: "Navi Portfolio",
   description: "Welcome to Navi's Portfolio Website",
+  icons: {
+    icon: "/favicon.ico",   
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
